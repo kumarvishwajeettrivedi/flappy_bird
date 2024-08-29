790c6284aa92c2db011c9de408b8d0070706c049
Trivedi_Vishwajeet <125482211+kumarvishwajeettrivedi@users.noreply.github.com>
2024-08-29 19:39:38 +0530

    Update script.js
    upp

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
-let pillar=document.querySelector(".pillar");
-let hole=document.querySelector(".hole");
-let character=document.querySelector(".character");
-let view=document.querySelector(".view");
+let pillar = document.querySelector(".pillar");
+let hole = document.querySelector(".hole");
+let character = document.querySelector(".character");
+let scoreDisplay = document.getElementById("score");
 
+let count = 0;
+let gravity = 3;
+let jumpHeight = -30;
+let gameInterval;
 
-let count=0;
-addEventListener("animationiteration",()=>{
-    var quard=-((Math.random()*300)+150);
-    hole.style.top=quard+"px";
-    
-    
+addEventListener("animationiteration", () => {
+    let randomTop = -((Math.random() * 250) + 150);
+    hole.style.top = randomTop + "px";
+    count++;
+    scoreDisplay.innerText = Math.floor(count / 2);
+});
 
-  count++;
+function startGame() {
+    gameInterval = setInterval(() => {
+        let charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        character.style.top = (charactertop + gravity) + "px";
 
-})
+        let pilla = parseInt(window.getComputedStyle(pillar).getPropertyValue("right"));
+        let holetop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+        let chole = -(500 - charactertop);
 
-setInterval(()=>{
-var charactertop=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-character.style.top=(charactertop+3)+"px";
-
-var pilla=parseInt(window.getComputedStyle(pillar).getPropertyValue("left"));
-var holetop=parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-var chole=-(500-charactertop)
-if((charactertop>500)||((pilla<20)&&(pilla>-50)&&((holetop>chole)||(chole<holetop)))){
-    alert("game over :score  ::"+count/2);
-    character.style.top=100+"px";
-    count=0;
+        if ((charactertop > 480) || ((pilla > 230) && (pilla < 290) && (charactertop < holetop || chole > holetop))) {
+            clearInterval(gameInterval);
+            alert("Game Over! Your score: " + Math.floor(count / 2));
+            resetGame();
+        }
+    }, 20);
 }
 
+function resetGame() {
+    character.style.top = "100px";
+    count = 0;
+    scoreDisplay.innerText = "0";
+    startGame();
+}
 
+addEventListener("keyup", () => {
+    let charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (charactertop + jumpHeight) + "px";
+});
 
-
-
-
-},30);
-
-addEventListener("keyup",()=>
-{
-
-        var push=parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-character.style.top=(push+(-30))+"px";
-
-
- 
-
-})
-
-
-
-
-
-
-// inside a function add evet listner and the n this property let x=clientx;
-//function fun(){}        let x=fun();
-//make a paralex website theme moon
+startGame();
